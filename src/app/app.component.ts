26d6a31dbc83c6a0f101ea74ceb24eac5ffb0d88
Patrick Flynn <pflynn210392@gmail.com>
2020-01-30 22:59:51 +0000

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
+import {ImageMap} from './imageMapResizer';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,13 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'ImageApp';
+  pigPart = '';
+
+  AppComponent() {
+    ImageMap('img[usemap]');
+  }
+
+  updatePigPart(part: string) {
+    this.pigPart = part;
+  }
 }
